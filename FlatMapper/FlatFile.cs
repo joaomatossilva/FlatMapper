821ef826f40fdac600b044c5d3a556b38be79f06
Joao Matos Silva <kappy@acydburne.com.pt>
2015-05-02 10:27:23 +0100

    Added the usage example on Readme

--- FlatMapper/FlatFile.cs
+++ FlatMapper/FlatFile.cs
@@ -1,77 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 
-namespace FlatMapper {
-	public class FlatFile<T> : IDisposable where T : new() {
-		private readonly Layout<T> layout;
+namespace FlatMapper
+{
+    public class FlatFile<T> where T : new()
+    {
+        private readonly Layout<T> layout;
 
-		private readonly Stream innerStream;
+        private readonly Stream innerStream;
 
-		private readonly Func<string, Exception, bool> handleEntryReadError;
+        private readonly Func<string, Exception, bool> handleEntryReadError;
 
-		public FlatFile(Layout<T> layout, Stream innerStream, Func<string, Exception, bool> handleEntryReadError) {
-			this.layout = layout;
-			this.innerStream = innerStream;
-			this.handleEntryReadError = handleEntryReadError;
-		}
+        public FlatFile(Layout<T> layout, Stream innerStream, Func<string, Exception, bool> handleEntryReadError)
+        {
+            this.layout = layout;
+            this.innerStream = innerStream;
+            this.handleEntryReadError = handleEntryReadError;
+        }
 
         public FlatFile(Layout<T> layout, Stream innerStream)
             : this(layout, innerStream, DefaultThrowExceptionOnReadError)
         {
         }
 
-	    private static bool DefaultThrowExceptionOnReadError(string line, Exception exception)
-	    {
-	        throw new Exception(string.Format("Error reading line '{0}'", line), exception);
-	    }
+        private static bool DefaultThrowExceptionOnReadError(string line, Exception exception)
+        {
+            throw new Exception(string.Format("Error reading line '{0}'", line), exception);
+        }
 
-		public IEnumerable<T> Read() {
-			var reader = new StreamReader(this.innerStream);
-			string line;
-			while ((line = reader.ReadLine()) != null) {
-				bool ignoreEntry = false;
-				T entry = default(T);
-				try {
-					entry = layout.ParseLine(line);
-				} catch (Exception ex) {
-					if (!handleEntryReadError(line, ex)) {
-						throw;
-					}
-					ignoreEntry = true;
-				}
-				if (!ignoreEntry) {
-					yield return entry;
-				}
-			}
-		}
+        public IEnumerable<T> Read()
+        {
+            using (var reader = new StreamReader(this.innerStream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    bool ignoreEntry = false;
+                    T entry = default(T);
+                    try
+                    {
+                        entry = layout.ParseLine(line);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (!handleEntryReadError(line, ex))
+                        {
+                            throw;
+                        }
+                        ignoreEntry = true;
+                    }
+                    if (!ignoreEntry)
+                    {
+                        yield return entry;
+                    }
+                }
+            }
+        }
 
-		public void Write(IEnumerable<T> entries) {
-			TextWriter writer = new StreamWriter(this.innerStream);
-			foreach (var entry in entries) {
-				var line = layout.BuildLine(entry);
-				writer.WriteLine(line);
-			}
-			writer.Flush();
-		}
-
-		public void Dispose() {
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-
-		protected void Dispose(bool disposing) {
-			if (disposing) {
-				if (this.innerStream != null) {
-					this.innerStream.Dispose();
-				}
-			}
-		}
-
-		~FlatFile() {
-			Dispose(false);
-		}
-	}
+        public void Write(IEnumerable<T> entries)
+        {
+            using (TextWriter writer = new StreamWriter(this.innerStream))
+            {
+                foreach (var entry in entries)
+                {
+                    var line = layout.BuildLine(entry);
+                    writer.WriteLine(line);
+                }
+                writer.Flush();
+            }
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 FlatMapper
 ==========
 
-FlatMapper is a library to import data from plain text files.
+FlatMapper is a library to import and export data from and to plain text files.
+
+
+## Features
+
++ It supports character delimited and fixed lenght files
++ Non intrusive - You don't have to chnage your code
++ Simple to use
+
+
+## How to use
+
+### Fixed Lenght Layout
+
+    var layout = new Layout<TestObject>.FixedLengthLayout()
+					.WithMember(o => o.Id, set => set.WithLenght(5).WithLeftPadding('0'))
+					.WithMember(o => o.Description, set => set.WithLenght(25).WithRightPadding(' '))
+					.WithMember(o => o.NullableInt, set => set.WithLenght(5).AllowNull("=Null").WithLeftPadding('0'));
+    
+
+### Delimited Layout
+
+    var layout = new Layout<TestObject>.DelimitedLayout()
+		            .WithDelimiter(";")
+		            .WithQuote("\"")
+		            .WithMember(o => o.Id, set => set.WithLenght(5).WithLeftPadding('0'))
+		            .WithMember(o => o.Description, set => set.WithLenght(25).WithRightPadding(' '))
+		            .WithMember(o => o.NullableInt, set => set.WithLenght(5).AllowNull("=Null").WithLeftPadding('0'));
+
+### Reading and Writing
+
+    //Reading data
+    using (var fileStream = File.OpenRead("c:\temp\data.txt"))
+    {
+        var flatfile = new FlatFile<TestObject>(layout, fileStream);
+        foreach(var objectInstance in flatfile.Read())
+        {
+            //Do Somethig....
+        }
+    }
+    
+    //Writing data
+    using (var fileStream = File.OpenWrite("c:\temp\data.txt"))
+    {
+        var flatfile = new FlatFile<TestObject>(layout, fileStream)
+        flatfile.Write(listOfObjects);
+    }
+
