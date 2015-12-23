da074717acf4a9ec3a71ab047d579d0325dccc0e
Joao Matos Silva <joao@kspace.pt>
2015-12-23 23:43:07 +0000

    Added missing files

--- FlatMapper.Tests/DelimitedFileTests.cs
+++ FlatMapper.Tests/DelimitedFileTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -26,12 +27,21 @@ namespace FlatMapper.Tests
                     .WithMember(o => o.Id, set => set.WithLength(5).WithLeftPadding('0'))
                     .WithMember(o => o.Description, set => set.WithLength(25).WithRightPadding(' '))
                     .WithMember(o => o.NullableInt, set => set.WithLength(5).AllowNull("=Null").WithLeftPadding('0'))
-                    .WithMember(o => o.NullableEnum, set => set.WithLength(10).AllowNull("").WithLeftPadding(' '));
+                    .WithMember(o => o.NullableEnum, set => set.WithLength(10).AllowNull("").WithLeftPadding(' '))
+                    .WithMember(o => o.Date, set => set.WithFormat(new CultureInfo("pt-PT")));
 
+            DateTime now = DateTime.UtcNow;
             objects = new List<TestObject>();
             for (int i = 1; i <= 10; i++)
             {
-                objects.Add(new TestObject { Id = i, Description = "Description " + i, NullableInt = i % 5 == 0 ? null : (int?)3, NullableEnum = i % 3 == 0 ? null : (Gender?)(i % 3) });
+                objects.Add(new TestObject
+                {
+                    Id = i,
+                    Description = "Description " + i,
+                    NullableInt = i % 5 == 0 ? null : (int?)3,
+                    NullableEnum = i % 3 == 0 ? null : (Gender?)(i % 3),
+                    Date = now.AddDays(i)
+                });
             }
         }
 
--- FlatMapper.Tests/TestObject.cs
+++ FlatMapper.Tests/TestObject.cs
@@ -21,13 +21,15 @@ namespace FlatMapper.Tests
 
         public Gender? NullableEnum { get; set; }
 
+        public DateTime Date { get; set; }
+
         public int GetHashCode(TestObject obj)
         {
             var idHash = Id.GetHashCode();
             var descriptionHash = Object.ReferenceEquals(Description, null) ? 0 : Description.GetHashCode();
             var nullableIntHash = !NullableInt.HasValue ? 0 : NullableInt.Value.GetHashCode();
             var nullableEnum = !NullableEnum.HasValue ? 0 : NullableEnum.Value.GetHashCode();
-            return idHash ^ descriptionHash ^ nullableIntHash ^ nullableEnum;
+            return idHash ^ descriptionHash ^ nullableIntHash ^ nullableEnum ^ Date.GetHashCode();
         }
 
         public bool Equals(TestObject other)
